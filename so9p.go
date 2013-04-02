d4af836b93888a7cd000ce0a13bfd25975d1b4e8
Ronald G. Minnich <rminnich@gmail.com>
2013-04-01 20:38:23 -0700

    A bit more Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- so9p.go
+++ so9p.go
@@ -5,15 +5,14 @@ import (
 	"log"
 	"net"
 	"os"
+	"path"
 	"net/rpc"
 )
 
 type fid int64
 
 type sfid struct {
-	Path string
-	File *os.File
-	Fid fid
+	Node Node
 }
 
 type So9ps struct {
@@ -54,6 +53,21 @@ type fileNode struct {
 
 var servermap map[fid] *sfid
 
+func (node *fileNode) Walk(walkTo string) (Node, error){
+	/* push a / onto the front of path. Then clean it.
+	 * This removes attempts to walk out of the tree.
+	 */
+	walkTo = path.Clean(path.Join("/", walkTo))
+	/* walk to whatever the new path is -- may be same as old */
+	fi, err := os.Stat(path.Join(node.Name, walkTo))
+	if err != nil {
+		return nil, err
+	}
+
+	newNode := &fileNode{fi.Name()}
+	return newNode, err
+}
+
 func (node *fileNode) FI() (os.FileInfo, error){
 	fi, err := os.Stat(node.Name)
 	return fi, err
@@ -75,38 +89,46 @@ func (server *So9ps) Srvattach(Args *Nameargs, Resp *Nameresp) (err error) {
 	n, err := server.Root()
 	Resp.FI[0], err = n.FI()
 	Resp.Fid[0] = Args.Fid[0]
-	servermap[Args.Fid[0]] = &sfid{Path:Args.Name[0], Fid: Args.Fid[0]}
+	servermap[Args.Fid[0]] = &sfid{n}
 	return err
 }
 	
 func (server *So9ps) Srvwalk(Args *Nameargs, Resp *Nameresp) (err error) {
+	var serverfid *sfid
+	var ok bool
 	fmt.Printf("Walk args %v resp %v\n", Args, Resp)
 	/* ofid valid? */
 	ofid := Args.Fid[0]
-	if sfid, ok := servermap[ofid]; ! ok {
+	if serverfid, ok = servermap[ofid]; ! ok {
 		return err
 	}
 
 	nfid := Args.Fid[1]
 
 	/* shortcut: new name is 0 length */
-	if len(Args.Name[0] == 0 {
+	if len(Args.Name[0]) == 0 {
 		servermap[nfid] = servermap[ofid]
 		return nil
 	}
+	n := serverfid.Node
 
+	walkTo := Args.Name[0]
 	/* walk to whatever the new path is -- may be same as old */
-	new
-	_, err = os.Stat(Args.Name[0])
-	if err != nil {
-		log.Print("Attach", err)
-		return err
+	if fs, ok := n.(interface {Walk(string)(Node, error)}); ok {
+		newNode, err := fs.Walk(walkTo)
+	Resp.FI[0], err = newNode.FI()
+	Resp.Fid[0] = Args.Fid[0]
+	servermap[Args.Fid[0]] = &sfid{newNode}
+		if err != nil {
+			log.Print("walk", err)
+			return err
+		}
 	}
 
-	n, err := server.Root()
-	Resp.FI[0], err = n.FI()
-	Resp.Fid[0] = Args.Fid[0]
-	servermap[Args.Fid[0]] = &sfid{Path:Args.Name[0], Fid: Args.Fid[0]}
+	if err != nil {
+		log.Print("Walk", err)
+	}
+
 	return err
 }
 	
