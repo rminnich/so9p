b7dcc2efb9ee9b13eb6cfd1fded7462725bb0a8d
Ronald G. Minnich <rminnich@gmail.com>
2013-04-17 14:11:19 -0700

    File IO works to arbitrary paths, once we walk them.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- so9p.go
+++ so9p.go
@@ -9,10 +9,10 @@ import (
 )
 
 var servermap map[fid]*sfid
-var clientfid fid
+var clientfid = fid(2)
 func main() {
 
-	if os.Args[1] == "s" {
+	if len(os.Args) > 1 && os.Args[1] == "s" {
 
 		servermap = make(map[fid]*sfid, 128)
 		S := new(So9ps)
@@ -30,16 +30,17 @@ func main() {
 		if err != nil {
 			log.Fatal("dialing:", err)
 		}
-		fi, err := client.attach("/", 1)
+		rootfid := fid(1)
+		fi, err := client.attach("/", rootfid)
 		if err != nil {
 			log.Fatal("attach", err)
 		}
-		newfid, fi, err := client.walk(1, "etc")
+		etcfid, fi, err := client.walk(rootfid, "etc")
 		if err != nil {
 			log.Fatal("walk", err)
 		}
-		fmt.Printf("Walk: %v, %v, %v\n", newfid, fi, err)
-		hostfid, fi, err := client.walk(newfid, "hosts")
+		fmt.Printf("Walk: %v, %v, %v\n", etcfid, fi, err)
+		hostfid, fi, err := client.walk(etcfid, "hosts")
 		if err != nil {
 			log.Fatal("walk", err)
 		}
@@ -54,6 +55,21 @@ func main() {
 			log.Fatal("read", err)
 		}
 		fmt.Printf("Read: %v, %v\n", data, err)
+		if len(os.Args) < 2 {
+			return
+		}
+		hostfid, fi, err = client.walk(rootfid, os.Args[1])
+		fmt.Printf("Walk to %v: %v, %v, %v\n", os.Args[1], 
+			hostfid, fi, err)
+		err = client.open(hostfid)
+		if err != nil {
+			log.Fatal("open", err)
+		}
+		_, err = client.read(hostfid, 1<<20, 0)
+		if err != nil {
+			log.Fatal("read", err)
+		}
+
 	}
 
 }
